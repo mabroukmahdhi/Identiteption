f4dbc5cfcef9418225af0a1f6ad0ed5f1e558822
Mabrouk Mahdhi <Mabrouk.Mahdhi@messer-is.com>
2022-05-23 20:05:39 +0200

    ShouldThrowServiceException -> PASS

---
+++ Identiteption.Tests.Unit/Services/Foundations/Identities/IdentityServiceTests.Logic.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Identiteption.Models.Identities.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using Xunit;
+
+namespace Identiteption.Tests.Unit.Services.Foundations.Identities
+{
+    public partial class IdentityServiceTests
+    {
+        [Theory]
+        [InlineData("DefaultError")]
+        [InlineData("LoginAlreadyAssociated")]
+        [InlineData("UserLockoutNotEnabled")]
+        [InlineData("UserNotInRole")]
+        public void ShouldThrowServiceException(string errorCode)
+        {
+            // given
+            string randomMessage = GetRandomString();
+            var inputIdentityResult = IdentityResult.Failed(new IdentityError()
+            {
+                Code = errorCode,
+                Description = randomMessage
+            });
+
+            // when
+            Action createAndThrowAction = () => this.identityService.CreateAndThrowIdentityException(inputIdentityResult);
+
+            // then
+
+            createAndThrowAction.Should().Throw<IdentityServiceException>();
+        }
+    }
+}
---
+++ Identiteption.Tests.Unit/Services/Foundations/Identities/IdentityServiceTests.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Identiteption.Services.Foundations.Identities;
+using Tynamix.ObjectFiller;
+
+namespace Identiteption.Tests.Unit.Services.Foundations.Identities
+{
+    public partial class IdentityServiceTests
+    {
+        private readonly IIdentityService identityService;
+
+        public IdentityServiceTests()
+        {
+            this.identityService = new IdentityService();
+        }
+
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+    }
+}
